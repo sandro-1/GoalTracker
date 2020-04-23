008c15610aad5b9da9b4c368e81a9ddf962d55fe
sandro-1 <mija.sand@gmail.com>
2020-04-22 22:31:30 -0700

    Add project files.

---
+++ GoalTracker/GoalTracker/ViewModels/CalendarPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using GoalTracker.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GoalTracker
+{
+    class CalendarPageViewModel : ContentPage, INotifyPropertyChanged
+    {
+
+        ICommand tapCommand;
+        public CalendarPageViewModel()
+        {
+            //GoToDetails = new Command(() =>
+            //{
+            //    DailyDetailsPage dailyPage = new DailyDetailsPage();
+            //    Navigation.PushAsync(dailyPage);
+            //});
+            tapCommand = new Command(OnTapped);
+        }
+
+        //public Command GoToDetails { get; }
+
+        public ICommand TapCommand
+        {
+            get { return tapCommand; }
+        }
+        async void OnTapped(object s)
+        {
+            DailyDetailsPage dailyPage = new DailyDetailsPage();
+
+            await Application.Current.MainPage.Navigation.PushAsync(dailyPage);
+        }
+        //public void TestTapped(object sender, EventArgs e) 
+        //{
+        //    //Navigation.PushAsync(new ContentPage { Content = new Label { Text = "Working" } }); 
+        //    string test = "test";
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
