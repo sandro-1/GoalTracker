49ab9bdd2a5c7ac834ab2ef146b7a55b036efdac
sandro-1 <mija.sand@gmail.com>
2020-05-02 20:38:58 -0700

    Add month variable and navigation arrows to nav bar and format both.

--- GoalTracker/GoalTracker/ViewModels/CalendarPageViewModel.cs
+++ GoalTracker/GoalTracker/ViewModels/CalendarPageViewModel.cs
@@ -6,22 +6,35 @@ using System.Diagnostics;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
-
 namespace GoalTracker
 {
-    class CalendarPageViewModel : ContentPage
+    class CalendarPageViewModel : ContentPage, INotifyPropertyChanged
     {
+        string month;
+        public string Month
+        {
+            get => month;
+            set
+            {
+                month = value;
+                var arg = new PropertyChangedEventArgs(nameof(Month));
+                PropertyChanged?.Invoke(this, arg);
+            }
+        }
         public CalendarPageViewModel()
         {            
             TapCommand = new Command(OnTapped);
+            Month = DateTime.Now.ToString("MMMM");
         }
-
+        
         public ICommand TapCommand { get; }
         async void OnTapped()
         {
             DailyDetailsPage dailyPage = new DailyDetailsPage();
             await Application.Current.MainPage.Navigation.PushAsync(dailyPage);
         }
-        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
     }
 }
